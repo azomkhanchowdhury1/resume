7394edcea0af229be5703be05616a7d2bbf6e486
Azom khan chowdhury <azomkhanchowdhury1@gmail.com>
2026-03-21 18:48:54 +0600

    Change Gmail SMTP from 587 TLS to 465 SSL to fix Render Timeout Crash

--- resume/settings.py
+++ resume/settings.py
@@ -142,8 +142,10 @@ STORAGES = {
 # Email Configuration (SMTP)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_TIMEOUT = 10
 EMAIL_HOST_USER = 'azomkhanchowdhury1@gmail.com'  # Replace with your Gmail
 EMAIL_HOST_PASSWORD = 'vjjo jjlr zjgm pcbz'  # Replace with your App Password
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
